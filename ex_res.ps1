f338276a451b4447c525752b4c8e2232287f6e18
philip-33 <philip.4242@gmail.com>
2017-10-23 01:31:20 -0700

    Comment edits
    Removed ref to forum Q that was used to learn PS syntax

--- ex_res.ps1
+++ ex_res.ps1
@@ -1,9 +1,6 @@
 # This script is for flipping (restarting) windows explorer.
 # Only tested with Win10Edu.1703
 # 
-# Origin https://stackoverflow.com/questions/5091345/stop-and-then-start-a-process-in-powershell
-# Special thanks to users 'chrissie1' and 'ravikanth'. 
-
 # Windows will normally attempt to restart explorer.exe automatically if it closes.
 
 Clear-host
