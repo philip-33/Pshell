e9a7b69f8628671e995e950de719303e35a710fb
user <user@localhost.localdomain>
2017-10-24 13:29:27 -0700

    Added comments and TODOs
    After the explorer process restarts, startup items are not processed normally so they need to be handled manually.

--- ex_res.ps1
+++ ex_res.ps1
@@ -1,7 +1,8 @@
-# This script is for flipping (restarting) windows explorer.
+# This script is for flipping (restarting) windows explorer, then executing the normal startup programs.
 # Only tested with Win10Edu.1703
 # 
-# Windows will normally attempt to restart explorer.exe automatically if it closes.
+# Windows will normally attempt to restart explorer.exe automatically if it closes,
+# but will not execute startup programs normally after this so it must be done manually.
 
 Clear-host
 Start-Sleep 1
@@ -12,3 +13,17 @@ if($process.Name -eq 'explorer')
     write-host "Explorer.exe is running. Stopping it now."
     Stop-Process -Name explorer
 }
+
+#TODO: Get list of programs set to run at startup
+# Pull list of startup programs and store it in a variable
+# This command pulls the current list of startup programs:
+#   $variable = Get-CimInstance Win32_StartupCommand | Select-Object Name, command, Location, User | Format-List 
+# the format-list option turns it into a human readable list, but this isn't necessary for the script itself
+
+#TODO: Run all of these programs based on that info
+# for each prog in $proglist, execute LOCATION/COMMAND as USER, echo NAME started
+# TODO: how to pull sub-items from $variable
+# TODO: how to execute programs from powershell
+# TODO: how to execute programs as a particular user
+#
+# loop through $variable[n] as long as $variable[n] exists {run $variable.Location/$variable.command as $variable.user}
