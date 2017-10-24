2f1a94696c29f7393a16e551b3137ffc9c4f9779
philip-33 <philip.4242@gmail.com>
2017-10-24 01:37:33 -0700

    Reduced autostart delay

--- ex_res.ps1
+++ ex_res.ps1
@@ -4,7 +4,7 @@
 # Windows will normally attempt to restart explorer.exe automatically if it closes.
 
 Clear-host
-Start-Sleep 3
+Start-Sleep 1
 $process = Get-Process explorer
 
 if($process.Name -eq 'explorer')
